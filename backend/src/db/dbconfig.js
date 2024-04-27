fdcb79458233a3509dfc23c7f7cca09e05135a94
Udara Nalawansa <udarakavishka13@gmail.com>
2024-04-27 16:37:59 +0530

    feat: mongodb connection establised
    connection through config file failed

---
+++ backend/src/db/dbconfig.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+const config= require('../config/config.js');
+const url= "mongodb+srv://admin123:6vN2U1skGY8BbLiS@cluster0.occgk0c.mongodb.net/?retryWrites=true&w=majority";
+ //const url= config.mongodb_url;
+
+const connectDB = () => {
+    mongoose.connect(url, {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        // useCreateIndex:true,
+        // useFindAndModify:false
+    }).then(()=>{
+        console.log("DB Connected");
+    }).catch((err)=>{
+        console.log("DB Connection Error: ", err);
+    });
+}
+
+module.exports={
+    connectDB
+} 
