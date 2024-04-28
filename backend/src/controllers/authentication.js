fd0dedf1feb1ad022f85d8944203ee9b5022dc5c
DilmyPerera <samadiniperera20@gmail.com>
2024-04-28 11:01:14 +0530

    feat: update authentication file
    setup file for login with exsting user

--- backend/src/controllers/authentication.js
+++ backend/src/controllers/authentication.js
@@ -17,3 +17,29 @@ const register = async (req,res,next)=>{
 };
 
 
+const login = async(req, res, next) =>{
+    const {username, password} =req.body;
+
+    try{
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(404).json({message: 'User not found'});
+        }
+
+        const passwordMatch = await user.comparePassword(password);
+        if(!passwordMatch){
+            return res.status(401).json({message: 'Incorrect password'});
+        }
+
+        const token = jwt.sign({ userId: user._id},process.env.SECRET_KEY,{
+            expiresIn: '1 hour'
+        });
+
+        register.json({ token });
+    }catch(error){
+        next(error);
+    }
+};
+
+
+
