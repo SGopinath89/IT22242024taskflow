7c6d239d8db82e1d6fce7fb06c74a57e15846d4a
DilmyPerera <samadiniperera20@gmail.com>
2024-05-01 20:13:07 +0530

    feat: import route files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express= require('express');
 const config = require('../backend/src/config/config.js');
 const database= require('../backend/src/db/dbconfig.js');
-
+const authRoutes = require ('../backend/src/routes/auth.js');
+const userRoutes = require('../backend/src/routes/user.js');
 const app=express();
 
 app.use(express.json());
@@ -11,3 +12,4 @@ database.connectDB();
 app.listen(config.port,()=>{
     console.log(`Server is up on port ${config.port}`);
 });
+
