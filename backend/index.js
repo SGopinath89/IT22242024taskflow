fb9375d749100f1b1f091516611df3e5384e6608
DilmyPerera <samadiniperera20@gmail.com>
2024-05-02 21:47:54 +0530

    fix: fix errors in the index file

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,18 @@ const database= require('../backend/src/db/dbconfig.js');
 const authRoutes = require ('../backend/src/routes/auth.js');
 const userRoutes = require('../backend/src/routes/user.js');
 const app=express();
+const port = process.env.PORT;
 
 app.use(express.json());
+ 
+app.use(express.urlencoded({extended: false}));
+
+
 
 database.connectDB();
+
+app.use('/api/auth',authRoutes);
+
 
 app.listen(config.port,()=>{
     console.log(`Server is up on port ${config.port}`);
