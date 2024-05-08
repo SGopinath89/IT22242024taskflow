023b41b13ff32f6741b3e3687bb0aa678a042b9d
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-08 21:18:22 +0530

    style: validation updated

--- backend/src/utils/validator.js
+++ backend/src/utils/validator.js
@@ -1,7 +1,7 @@
 const Validator = require('validatorjs');
-    const validator = async (body, rules, customMessages, callback) => {
-        const validation = new Validator(body, rules, customMessages);
-        validation.passes(() => callback(null, true));
-        validation.fails(() => callback(validation.errors, false));
-    };
-    module.exports = validator;
+const validator = async (body, rules, customMessages, callback) => {
+    const validation = new Validator(body, rules, customMessages);
+    validation.passes(() => callback(null, true));
+    validation.fails(() => callback(validation.errors, false));
+};
+module.exports = validator;
