f30fdfe4c6b4cb3916682bebe95074642213cc91
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-09 00:22:39 +0530

    feat: create board route

---
+++ backend/src/routes/board.router.js
@@ -0,0 +1,17 @@
+const express= require('express')
+const router= express.Router();
+const validation = require('../middleware/validation.js')
+const tokenHandle = require('../middleware/auth.middleware.js')
+const boardController = require('../controllers/board.controllers.js')
+
+
+router.post('/',tokenHandle,boardController.createBoard)
+
+router.get('/',tokenHandle,boardController.getAll)
+
+router.put('/',tokenHandle,boardController.updatePosition)
+
+router.get('/favourites',tokenHandle,boardController.getFavourites)
+
+router.put('/favourites',tokenHandle,boardController.updateFavouritePosition)
+
