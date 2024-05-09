baebdd9500f15b8a6f23d1e4d9e5aa76b968d45a
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-09 22:51:02 +0530

    feat: add update a section function

--- backend/src/controllers/section.controllers.js
+++ backend/src/controllers/section.controllers.js
@@ -15,7 +15,22 @@ const createSection= async(req,res)=>{
     }
 }
 
+const updateSection=async(req,res)=>{
+    const {sectionId}= req.params
+    try {
+        const section = await Section.findByIdAndUpdate(sectionId,{$set: req.body})
+        section._doc.tasks=[]
+        res.status(200).json(section)
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            success: false
+        })
+    }
+}
+
 module.exports={
-    createSection
-    
+    createSection,
+    updateSection
+
 }
