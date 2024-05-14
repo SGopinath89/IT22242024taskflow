68f83b04d360041b9db057de7e56f9b30d50fabc
DilmyPerera <samadiniperera20@gmail.com>
2024-05-14 20:54:03 +0530

    feat: update user model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,19 +1,55 @@
-const mongoose = require('mongoose')
-const { schemaOptions } = require('./modelOptions')
+// const mongoose = require('mongoose')
+// const { schemaOptions } = require('./modelOptions')
 
-const userSchema = new mongoose.Schema({
-  // userId:{
-  //   type: String
-  // },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-  }
-}, schemaOptions)
+// const userSchema = new mongoose.Schema({
+//   // userId:{
+//   //   type: String
+//   // },
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//   }
+// }, schemaOptions)
 
-module.exports = mongoose.model('User', userSchema)
+// module.exports = mongoose.model('User', userSchema)
+
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	surname: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	avatar: {
+		type: String,
+	},
+	color: {
+		type: String,
+	},
+	boards: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'board',
+		},
+	],
+});
+
+module.exports = mongoose.model('user', userSchema);
