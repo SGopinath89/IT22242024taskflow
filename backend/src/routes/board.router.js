899669c4099622c03920247e89396c3bd5cffcb7
DilmyPerera <samadiniperera20@gmail.com>
2024-05-14 21:52:35 +0530

    feat: update board.router file

--- backend/src/routes/board.router.js
+++ backend/src/routes/board.router.js
@@ -1,31 +1,47 @@
-const express= require('express')
-const router= express.Router();
-const validation = require('../middleware/validation.js')
-const tokenHandle = require('../middleware/auth.middleware.js')
-const boardController = require('../controllers/board.controllers.js')
+// const express= require('express')
+// const router= express.Router();
+// const validation = require('../middleware/validation.js')
+// const tokenHandle = require('../middleware/auth.middleware.js')
+// const boardController = require('../controllers/board.controllers.js')
 
 
-router.post('/',boardController.createBoard)
+// router.post('/',boardController.createBoard)
 
-router.get('/',tokenHandle,boardController.getAll)
+// router.get('/',tokenHandle,boardController.getAll)
 
-router.put('/',tokenHandle,boardController.updatePosition)
+// router.put('/',tokenHandle,boardController.updatePosition)
 
-router.get('/favourites',tokenHandle,boardController.getFavourites)
+// router.get('/favourites',tokenHandle,boardController.getFavourites)
 
-router.put('/favourites',tokenHandle,boardController.updateFavouritePosition)
+// router.put('/favourites',tokenHandle,boardController.updateFavouritePosition)
 
-router.get('/:boardId',
-// param('boardId').custom(value => {
-//     if (!validation.isObjectId(value)) {
-//       return Promise.reject('invalid id')
-//     } else return Promise.resolve()
-//   }),
-validation.validate,tokenHandle,boardController.getAll)
+// router.get('/:boardId',
+// // param('boardId').custom(value => {
+// //     if (!validation.isObjectId(value)) {
+// //       return Promise.reject('invalid id')
+// //     } else return Promise.resolve()
+// //   }),
+// validation.validate,tokenHandle,boardController.getAll)
 
-router.put('/:boardId',validation.validate,tokenHandle,boardController.updateBoard)
+// router.put('/:boardId',validation.validate,tokenHandle,boardController.updateBoard)
 
-router.delete('/:boardId',validation.validate,tokenHandle,boardController.deleteBoard)
+// router.delete('/:boardId',validation.validate,tokenHandle,boardController.deleteBoard)
 
-module.exports= router
+// module.exports= router
 
+
+
+const express = require('express');
+const boardController = require('../Controllers/boardController');
+const route = express.Router();
+
+route.post('/:boardId/add-member', boardController.addMember);
+route.put('/:boardId/update-background', boardController.updateBackground);
+route.put('/:boardId/update-board-description', boardController.updateBoardDescription);
+route.put('/:boardId/update-board-title', boardController.updateBoardTitle);
+route.post('/create', boardController.create);
+route.get('/:id', boardController.getById);
+route.get('/:id/activity', boardController.getActivityById);
+route.get('/', boardController.getAll);
+
+module.exports = route;
