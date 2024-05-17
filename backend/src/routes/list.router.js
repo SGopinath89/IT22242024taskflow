f09fb8c467fa67d612318aabf9132a79b59b9171
DilmyPerera <samadiniperera20@gmail.com>
2024-05-17 22:55:47 +0530

    feat: Defining Routes GET Route
    GET Route: Get All Lists by Board ID

--- backend/src/routes/list.router.js
+++ backend/src/routes/list.router.js
@@ -4,4 +4,5 @@ const listController = require('../controllers/list.controller.js');
 
 router.put('/:boardId/:listId/update-title', listController.updateListTitle);
 router.post('/create', listController.create);
+router.get('/:id', listController.getAll);
 
