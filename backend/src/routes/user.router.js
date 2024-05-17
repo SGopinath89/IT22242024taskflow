d2e67f03fcd4f5b4821277acd50e8d9c4e179c7e
DilmyPerera <samadiniperera20@gmail.com>
2024-05-17 23:10:04 +0530

    feat: export the router

--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -6,3 +6,5 @@ router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/get-user", userController.getUser);
 router.post("/get-user-with-email", userController.getUserWithMail);
+
+module.exports = router;
