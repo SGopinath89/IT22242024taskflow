3bafe1ca9a2175a8980b7731b56f92120e4f83cc
DilmyPerera <samadiniperera20@gmail.com>
2024-05-17 22:53:22 +0530

    feat: Defining Routes PUT Route
    PUT Route: Update List Title

--- backend/src/routes/list.router.js
+++ backend/src/routes/list.router.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const listController = require('../controllers/list.controller.js');
 
+router.put('/:boardId/:listId/update-title', listController.updateListTitle);
 
