8be67bf94f4b3fbafa7cddbca63cb1acbd7b4828
DilmyPerera <samadiniperera20@gmail.com>
2024-05-17 23:09:26 +0530

    feat: define the routes

--- backend/src/routes/user.router.js
+++ backend/src/routes/user.router.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const userController = require("../Controllers/userController");
 const router = express.Router();
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get("/get-user", userController.getUser);
+router.post("/get-user-with-email", userController.getUserWithMail);
