8bb8c6ca010008366d3c336fae0f0f29c8bb7b43
DilmyPerera <samadiniperera20@gmail.com>
2024-05-19 06:17:43 +0530

    feat: create "Create Function"

--- backend/src/services/list.services.js
+++ backend/src/services/list.services.js
@@ -1,3 +1,32 @@
 const listModel = require('../Models/listModel');
 const boardModel = require('../Models/boardModel');
 const cardModel = require('../Models/cardModel');
+
+const create = async (model, user, callback) => {
+	try {
+		
+		const tempList = await listModel(model);
+
+		const newList = await tempList.save();
+
+		const ownerBoard = await boardModel.findById(model.owner);
+       
+		ownerBoard.lists.push(newList.id);
+
+		ownerBoard.activity.unshift({
+			user: user._id,
+			name: user.name,
+			action: `added ${newList.title} to this board`,
+			color: user.color,
+		});
+
+		ownerBoard.save();
+
+		return callback(false, newList);
+	} catch (error) {
+		
+		return callback({ errMessage: 'Something went wrong', details: error.message });
+	}
+};
+
+
