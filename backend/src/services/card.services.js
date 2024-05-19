7a3aa2bbe1958d20104179e3cfb6b7c5fbc34ff7
DilmyPerera <samadiniperera20@gmail.com>
2024-05-19 07:01:03 +0530

    feat: import Dependencies

---
+++ backend/src/services/card.services.js
@@ -0,0 +1,7 @@
+const cardModel = require('../models/card.js');
+const listModel = require('../models/list.js');
+const boardModel = require('../models/board.js');
+const userModel = require('../models/user.js');
+const helperMethods = require('./helperMethod.js');
+
+
