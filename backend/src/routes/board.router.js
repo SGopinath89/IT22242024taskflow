1b97eecb3d03dbb210e011218677b13ad71f2aa7
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-20 02:34:54 +0530

    feat: board routes are updated

--- backend/src/routes/board.router.js
+++ backend/src/routes/board.router.js
@@ -34,18 +34,21 @@
 const express = require('express');
 const boardController = require('../controllers/board.controllers.js');
 const route = express.Router();
-
-route.post('/create', boardController.create);
-route.get('/', boardController.getAll);
+const {verifyToken}= require('../middleware/auth.middleware.js')
 
 
+route.post('/create',verifyToken, boardController.create);
+route.get('/',verifyToken, boardController.getAll);
+route.get('/:id', verifyToken, boardController.getById);
+route.get('/:id/activity',verifyToken, boardController.getActivityById);
 
-route.post('/:boardId/add-member', boardController.addMember);
-route.put('/:boardId/update-background', boardController.updateBackground);
-route.put('/:boardId/update-board-description', boardController.updateBoardDescription);
-route.put('/:boardId/update-board-title', boardController.updateBoardTitle);
 
-route.get('/:id', boardController.getById);
-route.get('/:id/activity', boardController.getActivityById);
+route.post('/:boardId/add-member', verifyToken,boardController.addMember);
+
+route.put('/:boardId/update-background', verifyToken,boardController.updateBackground);
+route.put('/:boardId/update-board-description',verifyToken, boardController.updateBoardDescription);
+route.put('/:boardId/update-board-title',verifyToken, boardController.updateBoardTitle);
+
+
 
 module.exports = route;
