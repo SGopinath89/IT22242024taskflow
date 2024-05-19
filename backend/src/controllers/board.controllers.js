936ac5f76a2898231a0f2c71fa41a17413a6a759
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-20 03:24:54 +0530

    fix: board controllers fixed
    add member function not working

--- backend/src/controllers/board.controllers.js
+++ backend/src/controllers/board.controllers.js
@@ -1,200 +1,3 @@
-// const Board = require('../models/board.js')
-// const Section = require('../models/section.js')
-// const Task = require('../models/task.js')
-// const User= require('../models/user.js')
-
-// const createBoard = async(req,res)=>{
-
-//     try {
-//         const boardsCount = await Board.find().count()
-//         const board = await Board.create({
-//             user: req.user._id,
-//             position: boardsCount > 0 ? boardsCount : 0
-//     })
-//     res.status(201).json(board)
-//     } catch (error) {
-//         return res.status(500).send({
-//             success: false,
-//             message: error.message
-//         })
-//     }
-// }
-
-
-// const getAll= async(req,res)=>{
-//     try {
-//         const boards= await Board.find({ user: req.user._id }).sort('-position')
-//         res.status(200).json(boards)
-//     } catch (error) {
-//         return res.status(500).json({
-//             message: error.message,
-//             success: false
-//         })
-//     }
-// }
-
-// const updatePosition= async(req, res)=>{
-//     const {boards}= req.body
-//     try {
-//         for(const key in boards.reverse()){
-//             const board= boards[key]
-//             await Board.findByIdAndUpdate(board.id,{
-//                 $set:{ position:key}
-//             })
-//         }
-//         res.status(200).json('Updated')
-//     } catch (error) {
-//         return res.status(500).json({
-//             message: error.message,
-//             success: false
-//         })
-//     }
-// }
-
-// const getOne= async(req,res)=>{
-//     const {boardId}= req.params
-//     try {
-//         const board= await Board.findOne({user: req.user._id, _id:boardId})
-//         if(!board){
-//             return res.status(404).json('Board not found')
-//         }else{
-//             for(const section of sections){
-//                 const tasks= await Task.find({section:section.id}).populate('section').sort('-position')
-//                 section._doc.tasks = tasks
-//             }
-//             board._doc.sections = sections
-//             res.status(200).json(board)
-//         }
-        
-//     } catch (error) {
-//         return res.status(500).json({
-//             message: error.message,
-//             success: false
-//         })
-//     }
-// }
-
-// const updateBoard= async(req,res)=>{
-//     const {boardId}=req.params
-//     const { title, description, favourite}= req.body
-
-//     try {
-//         if(title === ''){
-//             req.body.title='Untitled'
-//         }
-//         if(description === ''){
-//             req.body.description = 'Add description here'
-//         }
-//         const currentBoard = await Board.findById(boardId)
-//         if(!currentBoard){
-//             return res.status(404).json('Board not Found')
-//         }
-
-//         if(favourite !== undefined && currentBoard.favourite !== favourite){
-//             const favourite= await Board.find({user: currentBoard.user,favourite:true,_id:{$ne:boardId}}).sort('favouritePosition')
-//         if(favourite){
-//             req.body.favouritePosition = favourites.length > 0 ? favourites.length : 0
-//         }else{
-//             for(const key in favourite){
-//                 const element = favourites[key]
-//                 await Board.findByIdAndUpdate(element.id, { $set: { favouritePosition: key } })
-//             }
-//         }
-//         }
-//         const board = await Board.findByIdAndUpdate(boardId,{ $set: req.body })
-//         res.status(200).json(board,{ message: 'Board updated successfully', success: true });
-//     } catch (error) {
-//         return res.status(500).json({
-//             message: error.message,
-//             success: false
-//         })
-//     }
-// }
-
-// const updateFavouritePosition= async(req,res)=>{
-//     const {boards}= req.body
-//     try {
-//         for(const key in boards.reverse()){
-//             const board= boards[key]
-//             await Board.findByIdAndUpdate(board.id,{$set: { favouritePosition:key }})
-//         }
-//         res.status(200).json('updated Favourite ')
-//     } catch (error) {
-//         return res.status(500).json({
-//             message: error.message,
-//             success: false
-//         })
-//     }
-// }
-
-// const getFavourites= async(req,res)=>{
-//     try {
-//         const favourites= await Board.find({user: req.user._id, favourite:true}).sort('-favouritePosition')
-//         res.status(200).json(favourites)
-//     } catch (error) {
-//         return res.status(500).json({
-//             message: error.message,
-//             success: false
-//         })
-//     }
-// }
-
-// const deleteBoard= async(req,res)=>{
-//     const {boardId}= req.params
-//     try {
-//         const sections = await Section.find({ board: boardId })
-//         for (const section of sections) {
-//             await Task.deleteMany({ section: section.id })
-//         }
-//             await Section.deleteMany({ board: boardId })
-//         const currentBoard = await Board.findById(boardId)
-//         if (currentBoard.favourite) {
-//             const favourites = await Board.find({
-//                 user: currentBoard.user,
-//                 favourite: true,
-//                 _id: { $ne: boardId }
-//             }).sort('favouritePosition')
-
-//         for (const key in favourites) {
-//             const element = favourites[key]
-//             await Board.findByIdAndUpdate(
-//                 element.id,
-//                 { $set: { favouritePosition: key } }
-//             )
-//             }
-//         }
-//         await Board.deleteOne({ _id: boardId })
-
-//         const boards = await Board.find().sort('position')
-//         for (const key in boards) {
-//             const board = boards[key]
-//             await Board.findByIdAndUpdate(board.id,{ $set: { position: key } })
-//         }
-//         res.status(200).json({ message: 'Board deleted successfully', success: true })
-//     } catch (error) {
-//         return res.status(500).json({
-//             message: error.message,
-//             success: false
-//         })
-//     }
-// }
-
-
-
-
-// module.exports={
-//     createBoard,
-//     getAll,
-//     updatePosition,
-//     getOne,
-//     updateBoard,
-//     updateFavouritePosition,
-//     getFavourites,
-//     deleteBoard
-
-
-// }
-
 const boardService = require('../services/board.services.js' );
 
 // const create = async (req, res) => {
@@ -249,91 +52,111 @@ const getAll = async (req, res) => {
 };
 
 const getById = async (req, res) => {
-	// Validate whether params.id is in the user's boards or not
-	const validate = req.user.boards.filter((board) => board === req.params.id);
-	if (!validate)
-		return res.status(400).send({ errMessage: 'You can not show the this board, you are not a member or owner!' });
 
-	// Call the service
-	await boardService.getById(req.params.id, (err, result) => {
-		if (err) return res.status(400).send(err);
-		return res.status(200).send(result);
+	try {
+		const validate = req.user.boards.filter((board) => board === req.params.id);
+	if (!validate)
+		return res.status(400).send({ message: 'You are not authorized to view this board' });
+
+	await boardService.getById(req.params.id, (error, result) => {
+		if (error) return res.status(400).send(error);
 	});
+	} catch (error) {
+		return res.status(500).send({message: "An error occurred while fetching the boards.", error: error.message})
+	}
+	
 };
 
 const getActivityById = async (req, res) => {
-	// Validate whether params.id is in the user's boards or not
-	const validate = req.user.boards.filter((board) => board === req.params.id);
-	if (!validate)
-		return res.status(400).send({ errMessage: 'You can not show the this board, you are not a member or owner!' });
-
-	// Call the service
-	await boardService.getActivityById(req.params.id, (err, result) => {
-		if (err) return res.status(400).send(err);
-		return res.status(200).send(result);
-	});
+	try {
+		const validate = req.user.boards.filter((board) => board === req.params.id);
+		if (!validate)
+			return res.status(400).send({ Message: 'You are not authorized to view this board' });
+	
+		await boardService.getActivityById(req.params.id, (err, result) => {
+			if (err) return res.status(400).send(err);
+			return res.status(200).send(result);
+		});
+	} catch (error) {
+		return res.status(500).send({message: "An error occurred while fetching the boards.", error: error.message})
+	}
 };
 
 const updateBoardTitle = async (req, res) => {
+	try {
+		const validate = req.user.boards.filter((board) => board === req.params.id);
+		if (!validate)
+			return res.status(400).send({ errMessage: 'You are not authorized to access this board' });
 
-	const validate = req.user.boards.filter((board) => board === req.params.id);
-	if (!validate)
-		return res
-			.status(400)
-			.send({ errMessage: 'You can not change title of this board, you are not a member or owner!' });
-	const { boardId } = req.params;
-	const { title } = req.body;
-
-	await boardService.updateBoardTitle(boardId, title, req.user, (err, result) => {
-		if (err) return res.status(400).send(err);
-		return res.status(200).send(result);
-	});
+		const { boardId } = req.params;
+		const { title } = req.body;
+	
+		await boardService.updateBoardTitle(boardId, title, req.user, (error, result) => {
+			if (error) return res.status(400).send(error);
+			return res.status(200).send(result);
+		});
+	} catch (error) {
+		return res.status(500).send({message: "An error occurred while updating the board.", error: error.message})
+	}
+	
 };
 
 const updateBoardDescription = async (req, res) => {
+	try {
+		const validate = req.user.boards.filter((board) => board === req.params.id);
+		if (!validate)
+			return res.status(400).send({ errMessage: 'You are not authorized to access this board' });
 
-	const validate = req.user.boards.filter((board) => board === req.params.id);
-	if (!validate)
-		return res
-			.status(400)
-			.send({ errMessage: 'You can not change description of this board, you are not a member or owner!' });
-	const { boardId } = req.params;
-	const { description } = req.body;
+		const { boardId } = req.params;
+		const { description } = req.body;
+	
+		await boardService.updateBoardDescription(boardId, description, req.user, (error, result) => {
+			if (error) return res.status(400).send(error);
+			return res.status(200).send(result);
+		});
+		} catch (error) {
+		return res.status(500).send({message: "An error occurred while updating the board.", error: error.message})
+	}
 
-	await boardService.updateBoardDescription(boardId, description, req.user, (err, result) => {
-		if (err) return res.status(400).send(err);
-		return res.status(200).send(result);
-	});
+	
 };
 
 const updateBackground = async (req, res) => {
-	// Validate whether params.id is in the user's boards or not
-	const validate = req.user.boards.filter((board) => board === req.params.id);
-	if (!validate)
-		return res
-			.status(400)
-			.send({ errMessage: 'You can not change background of this board, you are not a member or owner!' });
-	const { boardId } = req.params;
-	const { background, isImage } = req.body;
-	// Call the service
-	await boardService.updateBackground(boardId, background, isImage, req.user, (err, result) => {
-		if (err) return res.status(400).send(err);
-		return res.status(200).send(result);
-	});
+	try {
+		const validate = req.user.boards.filter((board) => board === req.params.id);
+		if (!validate)
+			return res.status(400).send({ errMessage: 'You are not authorized to access this board' });
+
+		const { boardId } = req.params;
+		const { background, isImage } = req.body;
+
+		await boardService.updateBackground(boardId, background, isImage, req.user, (error, result) => {
+			if (error) return res.status(400).send(error);
+			return res.status(200).send(result);
+		});
+	} catch (error) {
+		return res.status(500).send({message: "An error occurred while updating the boards.", error: error.message})
+	}
+
+	
 };
 
 const addMember = async (req, res) => {
-	const validate = req.user.boards.filter((board) => board === req.params.id);
-	if (!validate)
-		return res.status(400).send({ errMessage: 'You can not add member to this board, you are not a member or owner!' });
-
-	const { boardId } = req.params;
-	const { members } = req.body;
+	try {
+		const validate = req.user.boards.filter((board) => board === req.params.id);
+		if (!validate)
+			return res.status(400).send({ errMessage: 'You are not authorized to access this board' });
 	
-	await boardService.addMember(boardId, members, req.user, (error, result) => {
-		if (error) return res.status(400).send({message:error.message});
-		return res.status(200).send(result);
-	});
+		const { boardId } = req.params;
+		const { members } = req.body;
+	
+		await boardService.addMember(boardId, members, req.user, (error, result) => {
+			if (error) return res.status(400).send({message:error.message});
+			return res.status(200).send(result);
+		});
+	} catch (error) {
+		return res.status(500).send({message: "An error occurred while adding members.", error: error.message})
+	}
 };
 
 module.exports = {
--- backend/src/services/board.services.js
+++ backend/src/services/board.services.js
@@ -2,68 +2,8 @@ const { findOne } = require('../models/board.js');
 const boardModel = require('../models/board.js');
 const userModel = require('../models/user.js');
 
-// const create = async (req, callback) => {
-// 	try {
-// 		const { title, backgroundImageLink, members } = req.body;
-// 		// Create and save new board
-// 		let newBoard = boardModel({ title, backgroundImageLink });
-// 		newBoard.save();
-
-// 		// Add this board to owner's boards
-// 		const user = await userModel.findById(req.user._id);
-// 		user.boards.unshift(newBoard.id);
-// 		await user.save();
-
-// 		// Add user to members of this board
-// 		let allMembers = [];
-// 		allMembers.push({
-// 			user: user.id,
-// 			name: user.name,
-// 			surname: user.surname,
-// 			email: user.email,
-// 			color: user.color,
-// 			role: 'owner',
-// 		});
-
-// 		// Save newBoard's id to boards of members and,
-// 		// Add ids of members to newBoard
-// 		await Promise.all(
-// 			members.map(async (member) => {
-// 				const newMember = await userModel.findOne({ email: member.email });
-// 				newMember.boards.push(newBoard._id);
-// 				await newMember.save();
-// 				allMembers.push({
-// 					user: newMember._id,
-// 					name: newMember.name,
-// 					surname: newMember.surname,
-// 					email: newMember.email,
-// 					color: newMember.color,
-// 					role: 'member',
-// 				});
-// 				//Add to board activity
-// 				newBoard.activity.push({
-// 					user: user.id,
-// 					name: user.name,
-// 					action: `added user '${newMember.name}' to this board`,
-// 				});
-// 			})
-// 		);
-
-// 		// Add created activity to activities of this board
-// 		newBoard.activity.unshift({ user: user._id, name: user.name, action: 'created this board', color: user.color });
-
-// 		// Save new board
-// 		newBoard.members = allMembers;
-// 		await newBoard.save();
-
-// 		return callback(false, newBoard);
-// 	} catch (error) {
-// 		return callback({
-// 			errMessage: 'Something went wrong',
-// 			details: error.message,
-// 		});
-// 	}
-// };
+//add member is not working
+//creation of board is not working properly
 
 const create = async (req, callback) => {
 	try {
@@ -137,14 +77,13 @@ const create = async (req, callback) => {
 	}
 };
 
-
 const getAll = async (userId, callback) => {
 	try {
 		const user = await userModel.findById(userId);
 		if(!user){
 			return callback({ message: 'User not found' });
 		}
-		console.log(userId)
+		// console.log(userId)
 		const boardIds = user.boards;
 
 		const boards = await boardModel.find({ _id: { $in: boardIds } });
@@ -164,23 +103,21 @@ const getById = async (id, callback) => {
 		const board = await boardModel.findById(id);
 		return callback(false, board);
 	} catch (error) {
-		return callback({ message: 'Something went wrong', details: error.message });
+		return callback({ message: 'Error fetching board', details: error.message });
 	}
 };
 
 const getActivityById = async (id, callback) => {
 	try {
-		// Get board by id
 		const board = await boardModel.findById(id);
 		return callback(false, board.activity);
 	} catch (error) {
-		return callback({ message: 'Something went wrong', details: error.message });
+		return callback({ message: 'Error fetching board ', details: error.message });
 	}
 };
 
 const updateBoardTitle = async (boardId, title, user, callback) => {
 	try {
-		// Get board by id
 		const board = await boardModel.findById(boardId);
 		board.title = title;
 		board.activity.unshift({
@@ -189,18 +126,19 @@ const updateBoardTitle = async (boardId, title, user, callback) => {
 			action: 'update title of this board',
 			color: user.color,
 		});
+
 		await board.save();
 		return callback(false, { message: 'Success!' });
 	} catch (error) {
-		return callback({ message: 'Something went wrong', details: error.message });
+		return callback({ message: 'Failed to update Board Title ', details: error.message });
 	}
 };
 
 const updateBoardDescription = async (boardId, description, user, callback) => {
 	try {
-		// Get board by id
 		const board = await boardModel.findById(boardId);
 		board.description = description;
+
 		board.activity.unshift({
 			user: user._id,
 			name: user.name,
@@ -210,7 +148,7 @@ const updateBoardDescription = async (boardId, description, user, callback) => {
 		await board.save();
 		return callback(false, { message: 'Success!' });
 	} catch (error) {
-		return callback({ message: 'Something went wrong', details: error.message });
+		return callback({ message: 'Failed to update Board Description', details: error.message });
 	}
 };
 
@@ -221,7 +159,6 @@ const updateBackground = async (id, background, isImage, user, callback) => {
 		board.backgroundImageLink = background;
 		board.isImage = isImage;
 
-		// Log the activity
 		board.activity.unshift({
 			user: user._id,
 			name: user.name,
@@ -229,18 +166,21 @@ const updateBackground = async (id, background, isImage, user, callback) => {
 			color: user.color,
 		});
 
-		// Save changes
 		await board.save();
 
 		return callback(false, board);
 	} catch (error) {
-		return callback({ message: 'Something went wrong', details: error.message });
+		return callback({ message: 'Failed to update board background', details: error.message });
 	}
 };
 
 const addMember = async (id, members, user, callback) => {
 	try {
 		const board = await boardModel.findById(id);
+
+		if(!board){
+			return callback({message:"board not found"});
+		}
 
 		await Promise.all(
 			members.map(async (member) => {
@@ -268,9 +208,11 @@ const addMember = async (id, members, user, callback) => {
 
 		return callback(false, board.members);
 	} catch (error) {
-		return callback({ message: 'Something went wrong', details: error.message });
+		return callback({ message: 'failed to add members', details: error.message });
 	}
 };
+
+
 
 module.exports = {
 	create,
