e92a2c1248afdc821a78a1cd3e899d412a6dd154
DilmyPerera <samadiniperera20@gmail.com>
2024-05-19 07:16:47 +0530

    feat: create function to update comment on a card

--- backend/src/services/card.services.js
+++ backend/src/services/card.services.js
@@ -149,3 +149,45 @@ const addComment = async (cardId, listId, boardId, user, body, callback) => {
 		return callback({ errMessage: 'Something went wrong', details: error.message });
 	}
 };
+
+const updateComment = async (cardId, listId, boardId, commentId, user, body, callback) => {
+	try {
+		
+        
+		const card = await cardModel.findById(cardId);
+		const list = await listModel.findById(listId);
+		const board = await boardModel.findById(boardId);
+        
+		const validate = await helperMethods.validateCardOwners(card, list, board, user, false);
+		if (!validate) {
+			errMessage: 'You dont have permission to update this card';
+		}
+
+		card.activities = card.activities.map((activity) => {
+			if (activity._id.toString() === commentId.toString()) {
+				if (activity.userName !== user.name) {
+					return callback({ errMessage: "You can not edit the comment that you hasn't" });
+				}
+				activity.text = body.text;
+			}
+			return activity;
+		});
+		await card.save();
+
+
+		board.activity.unshift({
+			user: user._id,
+			name: user.name,
+			action: body.text,
+			actionType: 'comment',
+			edited: true,
+			color: user.color,
+			cardTitle: card.title,
+		});
+		board.save();
+
+		return callback(false, { message: 'Success!' });
+	} catch (error) {
+		return callback({ errMessage: 'Something went wrong', details: error.message });
+	}
+};
