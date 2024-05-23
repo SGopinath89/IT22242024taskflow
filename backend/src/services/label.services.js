e73803bd94a27bc6f7a9aee1fba7c9737f0fd2f4
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-24 01:14:03 +0530

    feat: add label services

---
+++ backend/src/services/label.services.js
@@ -0,0 +1,111 @@
+const cardModel = require('../models/card.js');
+const listModel = require('../models/list.js');
+const boardModel = require('../models/board.js');
+const userModel = require('../models/user.js');
+const helperMethods = require('./helperMethod.js');
+
+
+const createLabel = async (cardId, listId, boardId, user, label, callback) => {
+	try {
+		const card = await cardModel.findById(cardId);
+		const list = await listModel.findById(listId);
+		const board = await boardModel.findById(boardId);
+
+		const validate = await helperMethods.validateCardOwners(card, list, board, user, false);
+		if (!validate) {return res.status(400).send({ message: 'You are not authorized to add label to the card' });}
+
+		card.labels.unshift({
+			text: label.text,
+			color: label.color,
+			backcolor: label.backColor,
+			selected: true,
+		});
+		await card.save();
+
+		const labelId = card.labels[0]._id;
+
+		return callback(false, { labelId: labelId });
+	} catch (error) {
+		return callback({ errMessage: 'Error adding a label to the card', details: error.message });
+	}
+};
+
+const updateLabel = async (cardId, listId, boardId, labelId, user, label, callback) => {
+	try {
+		const card = await cardModel.findById(cardId);
+		const list = await listModel.findById(listId);
+		const board = await boardModel.findById(boardId);
+
+		const validate = await helperMethods.validateCardOwners(card, list, board, user, false);
+		if (!validate) {return res.status(400).send({ message: 'You are not authorized to add label to the card' })}
+
+		card.labels = card.labels.map((item) => {
+			if (item._id.toString() === labelId.toString()) {
+				item.text = label.text;
+				item.color = label.color;
+				item.backColor = label.backColor;
+			}
+			return item;
+		});
+		await card.save();
+
+		return callback(false, { message: 'Success!' });
+	} catch (error) {
+		return callback({ errMessage: 'Error updating a label in the card', details: error.message });
+	}
+};
+
+const deleteLabel = async (cardId, listId, boardId, labelId, user, callback) => {
+	try {
+		const card = await cardModel.findById(cardId);
+		const list = await listModel.findById(listId);
+		const board = await boardModel.findById(boardId);
+
+		const validate = await helperMethods.validateCardOwners(card, list, board, user, false);
+		if (!validate) {
+			return res.status(400).send({ message: 'You are not authorized to delete the label' });
+		}
+
+		card.labels = card.labels.filter((label) => label._id.toString() !== labelId.toString());
+		await card.save();
+
+		return callback(false, { message: 'Success!' });
+	} catch (error) {
+		return callback({ errMessage: 'Error deleting a label in the card', details: error.message });
+	}
+};
+
+const updateLabelSelection = async (cardId, listId, boardId, labelId, user, selected, callback) => {
+	try {
+		// Get models
+		const card = await cardModel.findById(cardId);
+		const list = await listModel.findById(listId);
+		const board = await boardModel.findById(boardId);
+
+		// Validate owner
+		const validate = await helperMethods.validateCardOwners(card, list, board, user, false);
+		if (!validate) {
+			return res.status(400).send({ message: 'You are not authorized to access the label' });
+		}
+
+		//Update label
+		card.labels = card.labels.map((item) => {
+			if (item._id.toString() === labelId.toString()) {
+				item.selected = selected;
+			}
+			return item;
+		});
+		await card.save();
+
+		return callback(false, { message: 'Success!' });
+	} catch (error) {
+		return callback({ errMessage: 'Error selecting a label in the card', details: error.message });
+	}
+};
+
+module.exports={
+    createLabel,
+    updateLabel,
+    deleteLabel,
+    updateLabelSelection
+}
