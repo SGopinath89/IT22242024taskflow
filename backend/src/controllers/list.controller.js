5920d1d09f656eda705923b822ebb1805979e3b5
Udara Nalawansa <udarakavishka13@gmail.com>
2024-05-24 02:21:27 +0530

    feat: add update list order controller

--- backend/src/controllers/list.controller.js
+++ backend/src/controllers/list.controller.js
@@ -3,14 +3,14 @@ const listService = require('../services/list.services.js');
 const create = async (req, res) => {
 	const { title, boardId } = req.body;
 
-	if (!(title && boardId)) return res.status(400).send({ errMessage: 'Title cannot be empty' });
+	if (!(title && boardId)) return res.status(400).send({ Message: 'Title cannot be empty' });
 
 	const validate = req.user.boards.filter((board) => board === boardId);
 	if (!validate)
 		return res.status(400).send({ Message: 'You are not authorized to access  ' });
 
-	await listService.create({ title: title, owner: boardId }, req.user, (err, result) => {
-		if (err) return res.status(500).send(err);
+	await listService.create({ title: title, owner: boardId }, req.user, (error, result) => {
+		if (error) return res.status(500).send(error);
 		return res.status(201).send(result);
 	});
 };
@@ -22,8 +22,8 @@ const getAll = async (req, res) => {
 	if (!validate)
 		return res.status(400).send({ Message: 'You are not authorized to get list ' });
 
-	await listService.getAll(boardId, (err, result) => {
-		if (err) return res.status(500).send(err);
+	await listService.getAll(boardId, (error, result) => {
+		if (error) return res.status(500).send(error);
 		return res.status(200).send(result);
 	});
 };
@@ -34,17 +34,32 @@ const deleteById = async (req, res) => {
 
     if (!(listId && boardId)) return res.status(400).send({ Message: 'List or board undefined' });
 
-	await listService.deleteById(listId, boardId, user, (err, result) => {
-		if (err) return res.status(500).send(err);
+	await listService.deleteById(listId, boardId, user, (error, result) => {
+		if (error) return res.status(500).send(error);
+		return res.status(200).send(result);
+	});
+};
+
+const updateCardOrder = async (req, res) => {
+	const { boardId, sourceId, destinationId, destinationIndex, cardId } = req.body;
+	const user = req.user;
+
+	if (!(boardId && sourceId && destinationId && cardId))
+		return res.status(400).send({ Message: 'All parameters not provided' });
+
+	const validate = user.boards.filter((board) => board === boardId);
+	if (!validate) return res.status(403).send({ Message: 'You are not authorized to update Order' });
+
+	await listService.updateCardOrder(boardId, sourceId, destinationId, destinationIndex, cardId, user, (error, result) => {
+		if (error) return res.status(500).send(error);
 		return res.status(200).send(result);
 	});
 };
 
 
-
-
 module.exports={
     create,
     getAll,
-    deleteById
+    deleteById,
+    updateCardOrder
 }
