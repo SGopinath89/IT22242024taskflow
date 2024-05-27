2920df64c96f5a33727ba6350ac769680a2644ba
DilmyPerera <samadiniperera20@gmail.com>
2024-05-27 15:14:03 +0530

    Revert "fix: correction of the paths"
    This reverts commit efd5ba3c0ab63d9f549a29e7084e2c4f3ca90f73.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
-import Index from "../src/Components/Pages/IndexPage/Index.js";
-import Login from "../src/Components/Pages/LoginPage/Login.js";
-import Register from "../src/Components/Pages/RegisterPage/Register.js";
-import Alert from "../src/Components/AlertSnackBar.js";
+import Index from "./Components/Pages/IndexPage/Index";
+import Login from "./Components/Pages/LoginPage/Login";
+import Register from "./Components/Pages/RegisterPage/Register";
+import Alert from "./Components/AlertSnackBar";
 import { BrowserRouter, Switch } from "react-router-dom";
-import Boards from "../src/Components/Pages/BoardsPage/Boards.js";
+import Boards from "./Components/Pages/BoardsPage/Boards";
 import ProtectedRoute from "./Utils/ProtectedRoute";
 import { loadUser } from "./Services/userService";
 import Store from "./Redux/Store";
